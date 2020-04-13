cfdde643b977b4c12f78c3f12549e523982a5440
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-13 09:54:18 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,63 +1,36 @@
 class Book
-<<<<<<< HEAD
   attr_accessor :title, :author, :page_count, :genre
-=======
->>>>>>> f9811ac8ca588c84cd1ecc89ef9cdbc7136eb490
     def initialize(title)
         @title = title
     end
 
-<<<<<<< HEAD
     # def title
     #     @title
     # end
-=======
-    def title
-        self.title
-    end
->>>>>>> f9811ac8ca588c84cd1ecc89ef9cdbc7136eb490
 
     def author=(author)
         @author = author
     end
 
-<<<<<<< HEAD
     # def author
     #     @author
     # end
-=======
-    def author
-        @author
-    end
->>>>>>> f9811ac8ca588c84cd1ecc89ef9cdbc7136eb490
 
     def page_count=(num)
         @page_count = num
     end
 
-<<<<<<< HEAD
     # def page_count
     #     @page_count
     # end
-=======
-    def page_count
-        @page_count
-    end
->>>>>>> f9811ac8ca588c84cd1ecc89ef9cdbc7136eb490
 
     def genre=(genre)
         @genre = genre
     end
 
-<<<<<<< HEAD
     # def genre
     #     @genre
     # end
-=======
-    def genre
-        @genre
-    end
->>>>>>> f9811ac8ca588c84cd1ecc89ef9cdbc7136eb490
 
     def turn_page
         puts "Flipping the page...wow, you read fast!"
